7953ba53e90bdd814cb8d073057dada8535102f5
Phariq <pharisnjuguna22@gmail.com>
2022-11-24 22:52:03 +0300

    edited styles and main indexes - added preloader  and date with Javascript

---
+++ SJSS FILES/main.js
@@ -0,0 +1,13 @@
+// Automatically update year
+
+const date = document.getElementById('date');
+
+date.innerHTML = new Date().getFullYear();
+
+// pre loader
+
+const preloader = document.querySelector('.pre-loader');
+
+window.addEventListener('load', () => {
+    preloader.classList.add('hide-preloader');
+})
